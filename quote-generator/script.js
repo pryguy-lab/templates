130dd4de2d28ed93a9b88ab44d91042d8e103bb4
Mike Pry <mikepry@hotmail.com>
2021-02-05 08:35:09 -0500

    added a proxy server for CORS

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
+// Get Quote from API
+async function getQuote() {
+  const proxyUrl = "https://sleepy-headland-68644.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // if Author is blank, add "unknown Author"
+    if (data.quoteAuthor.text === "") {
+      authorText.innerText = "Unknown Author";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    authorText.innerText = data.quoteAuthor;
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    getQuote();
+  }
+}
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+//Event Listeners
+
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+
+getQuote();
