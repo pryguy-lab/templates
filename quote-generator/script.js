4b0ddbc4d59cb3332ea084489dec01bf9e8ae129
Mike Pry <mikepry@hotmail.com>
2021-02-09 00:10:46 -0500

    modified css

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,9 +3,22 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
+function showloadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 // Get Quote from API
 async function getQuote() {
+  showloadingSpinner();
   const proxyUrl = "https://sleepy-headland-68644.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -17,6 +30,7 @@ async function getQuote() {
       authorText.innerText = "Unknown Author";
     } else {
       authorText.innerText = data.quoteAuthor;
+      console.log(data.quoteText);
     }
     authorText.innerText = data.quoteAuthor;
     // Reduce font size for long quotes
@@ -26,8 +40,10 @@ async function getQuote() {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    //Stop Loader, Show Quote
+    removeLoadingSpinner();
   } catch (error) {
-    getQuote();
+    getQuote(e);
   }
 }
 // Tweet Quote
